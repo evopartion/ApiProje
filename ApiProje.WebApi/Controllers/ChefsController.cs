ec3c621eb58ec6138ca553dd2ed47b57ba87f726
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-19 04:14:29 +0300

    Chef Api İşlemleri

---
+++ ApiProje.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,52 @@
+﻿using ApiProje.WebApi.Context;
+using ApiProje.WebApi.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProje.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Şef sisteme başarıyla eklendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return Ok("Şef sistemden silindi");
+        }
+        [HttpGet("GetChef")]
+        public IActionResult GetChef(int id)
+        {
+            return Ok(_context.Chefs.Find(id));
+        }
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Şef güncelleme işlemi başarılı");
+        }
+
+    }
+}
