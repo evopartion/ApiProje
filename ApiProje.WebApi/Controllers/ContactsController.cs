d43ced63fd511e1b2e7aae8182e2635a8db23451
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-20 20:38:48 +0300

    Dto ile Manuel Mapleme İşlemi

---
+++ ApiProje.WebApi/Controllers/ContactsController.cs
@@ -0,0 +1,39 @@
+﻿using ApiProje.WebApi.Context;
+using ApiProje.WebApi.Dtos.ContactDtos;
+using ApiProje.WebApi.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProje.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ContactsController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = _context.Contacts.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+        {
+            Contact contact=new Contact();
+            contact.Email = createContactDto.Email;
+            contact.Address = createContactDto.Address;
+            contact.Phone = createContactDto.Phone;
+            contact.MapLocation = createContactDto.MapLocation;
+            contact.OpenHours = createContactDto.OpenHours;
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("Ekleme İşlemi Başarılı");
+        }
+    }
+}
