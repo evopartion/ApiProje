5887dc246140ccdc9e2d09cd4232a0732584c42f
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-20 21:10:08 +0300

    General Mapping ve Auto Mapper

---
+++ ApiProje.WebApi/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using ApiProje.WebApi.Dtos.FeatureDtos;
+using ApiProje.WebApi.Dtos.MessageDtos;
+using ApiProje.WebApi.Entity;
+using AutoMapper;
+
+namespace ApiProje.WebApi.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+             CreateMap<Feature,ResultFeatureDto>().ReverseMap();
+             CreateMap<Feature,CreateFeatureDto>().ReverseMap();
+             CreateMap<Feature,UpdateFeatureDto>().ReverseMap();
+             CreateMap<Feature,GeyByIdFeatureDto>().ReverseMap();
+             
+             CreateMap<Message,ResultMessageDto>().ReverseMap();
+             CreateMap<Message,CreateMessageDto>().ReverseMap();
+             CreateMap<Message,UpdateMessageDto>().ReverseMap();
+             CreateMap<Message,GetByIdMessageDto>().ReverseMap();
+        }
+    }
+}
